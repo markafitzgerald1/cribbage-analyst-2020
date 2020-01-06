0abb96c5b681c6649d2fa8d5d10b41a16959595b
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-01-05 23:27:12 -0500

    Add missing MPL 2 License Headers

--- like_button.js
+++ like_button.js
@@ -1,7 +1,10 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 "use strict";
 
-var React = require("react");
-var ReactDOM = require("react-dom");
+const React = require("react");
+const ReactDOM = require("react-dom");
 
 const e = React.createElement;
 
