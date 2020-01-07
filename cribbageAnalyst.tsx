e95506373c943237e1904ae6c0ec43b0b85579d5
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-01-07 00:56:34 -0500

    Add player count to game

--- cribbageAnalyst.tsx
+++ cribbageAnalyst.tsx
@@ -1,9 +1,27 @@
 "use strict";
 
 import * as React from "react";
-import { render } from "react-dom";
+import * as ReactDOM from "react-dom";
 
-render(
-  <div>Hello, Player 1!</div>,
+interface GameState {
+  playerCount: number;
+}
+
+class Game extends React.Component<GameState> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return [...Array(this.props.playerCount)].map((_, baseZeroPlayerNumber) => (
+      <div key={"player-" + baseZeroPlayerNumber}>
+        Hello, Player {baseZeroPlayerNumber + 1}!
+      </div>
+    ));
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Game, { playerCount: 2 }),
   document.querySelector("#cribbage_analyst")
 );
