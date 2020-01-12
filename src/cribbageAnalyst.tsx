242f4d5e370e433f71dd617d0f7ec86ec151fe26
Mark A. Fitzgerald <mark.a.fitzgerald1@gmail.com>
2020-01-11 23:49:40 -0500

    Replace game player count with dealt hand
    Switch initial focus to most often user requested feature: discard
    analysis.

--- src/cribbageAnalyst.tsx
+++ src/cribbageAnalyst.tsx
@@ -5,26 +5,72 @@
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { List } from "immutable";
 
-interface GameState {
-  playerCount: number;
+enum Index {
+  ACE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  JACK,
+  QUEEN,
+  KING
 }
 
-class Game extends React.Component<GameState> {
+enum Suit {
+  CLUBS,
+  DIAMONDS,
+  HEARTS,
+  SPADES
+}
+
+interface Card {
+  index: Index;
+  suit: Suit;
+}
+
+interface DealtHand {
+  cards: List<Card>;
+}
+
+class DealtHandComponent extends React.Component<DealtHand> {
+  render() {
+    return this.props.cards
+      .map(card => (
+        <div key={`${card.index}-${card.suit}`}>
+          Index {card.index} - Suit {card.suit}
+        </div>
+      ))
+      .toArray();
+  }
+}
+
+class Game extends React.Component<DealtHand> {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return [...Array(this.props.playerCount)].map((_, baseZeroPlayerNumber) => (
-      <div key={"player-" + baseZeroPlayerNumber}>
-        Hello, Player {baseZeroPlayerNumber + 1}!
-      </div>
-    ));
+    return <DealtHandComponent cards={this.props.cards}></DealtHandComponent>;
   }
 }
 
 ReactDOM.render(
-  React.createElement(Game, { playerCount: 2 }),
+  React.createElement(Game, {
+    cards: List.of(
+      { index: Index.TWO, suit: Suit.CLUBS },
+      { index: Index.THREE, suit: Suit.DIAMONDS },
+      { index: Index.FOUR, suit: Suit.HEARTS },
+      { index: Index.EIGHT, suit: Suit.SPADES },
+      { index: Index.NINE, suit: Suit.CLUBS },
+      { index: Index.TEN, suit: Suit.DIAMONDS }
+    )
+  }),
   document.querySelector("#cribbage_analyst")
 );
